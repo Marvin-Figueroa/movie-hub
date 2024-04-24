f897b1d8c703b9d6afa74f443558c9fa896040fb
Marvin Figueroa <marvinfigueroa2012@gmail.com>
2024-04-24 17:38:08 -0600

    Build a responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
-import { DatePicker } from 'antd'
+import { Layout } from 'antd'
+import { Header, Content } from 'antd/es/layout/layout'
+import Sider from 'antd/es/layout/Sider'
 
 function App() {
-  return <DatePicker />
+  return (
+    <Layout style={{ height: '100vh' }}>
+      <Header style={{ backgroundColor: 'tomato' }}>Header</Header>
+      <Layout>
+        <Sider
+          trigger={null}
+          breakpoint='sm'
+          collapsedWidth={0}
+          width='25%'
+          style={{ backgroundColor: 'gold' }}>
+          Sider
+        </Sider>
+        <Content style={{ backgroundColor: 'royalblue' }}>Content</Content>
+      </Layout>
+    </Layout>
+  )
 }
 
 export default App

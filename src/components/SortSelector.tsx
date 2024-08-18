13f561b7f66455f1cb05469e29ae1214744f73c0
Marvin Figueroa <marvinfigueroa2012@gmail.com>
2024-08-18 14:51:22 -0600

    Build sort selector

---
+++ src/components/SortSelector.tsx
@@ -0,0 +1,30 @@
+import { Select } from "antd";
+
+interface Props {
+  onSelectSort: (sort: string) => void;
+  selectedSort: string;
+}
+
+const SortSelector = ({ onSelectSort, selectedSort }: Props) => {
+  const options = [
+    { value: "title.asc", label: "Title" },
+    { value: "revenue.desc", label: "Revenue" },
+    { value: "popularity.desc", label: "Popularity" },
+    { value: "vote_average.desc", label: "Average Rating" },
+    { value: "primary_release_date.desc", label: "Release Date" },
+  ];
+
+  return (
+    <Select
+      defaultValue={options.find(
+        (option) => option.value === "popularity.desc"
+      )}
+      value={options.find((option) => option.value === selectedSort)}
+      onSelect={(_, option) => onSelectSort(option.value)}
+      style={{ width: "200px" }}
+      options={options}
+    />
+  );
+};
+
+export default SortSelector;

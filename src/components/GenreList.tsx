ec9da32b55a99e7487a51c8fa657c913137d1a49
Marvin Figueroa <marvinfigueroa2012@gmail.com>
2024-08-19 13:01:03 -0600

    Add heading to genres list

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,4 +1,4 @@
-import { List, Image, Space, Button } from "antd";
+import { List, Image, Space, Button, Typography } from "antd";
 import { useGenres } from "../hooks/useGenres";
 
 interface Props {
@@ -10,37 +10,45 @@ const GenreList = ({ onSelectGenre, selectedGenre }: Props) => {
   const { data: genres } = useGenres();
 
   return (
-    <List
-      size="small"
-      bordered
-      dataSource={genres}
-      renderItem={(genre) => (
-        <List.Item>
-          <Space align="center" size="small">
-            <Image
-              style={{ borderRadius: "8px", border: "2px solid #15B7DB" }}
-              preview={false}
-              width={50}
-              height={70}
-              src={genre.imgSrc}
-            />
-            <Button
-              size="large"
-              onClick={() => onSelectGenre(genre.id)}
-              type="link"
-              style={{
-                color: "#15B7DB",
-                fontWeight: `${
-                  selectedGenre === genre.id ? "bolder" : "normal"
-                }`,
-              }}
-            >
-              {genre.name}
-            </Button>
-          </Space>
-        </List.Item>
-      )}
-    />
+    <>
+      <Typography.Title
+        level={2}
+        style={{ margin: "0 0 20px 20px", color: "#15B7DB" }}
+      >
+        Genres
+      </Typography.Title>
+      <List
+        size="small"
+        bordered
+        dataSource={genres}
+        renderItem={(genre) => (
+          <List.Item>
+            <Space align="center" size="small">
+              <Image
+                style={{ borderRadius: "8px", border: "2px solid #15B7DB" }}
+                preview={false}
+                width={50}
+                height={70}
+                src={genre.imgSrc}
+              />
+              <Button
+                size="large"
+                onClick={() => onSelectGenre(genre.id)}
+                type="link"
+                style={{
+                  color: "#15B7DB",
+                  fontWeight: `${
+                    selectedGenre === genre.id ? "bolder" : "normal"
+                  }`,
+                }}
+              >
+                {genre.name}
+              </Button>
+            </Space>
+          </List.Item>
+        )}
+      />
+    </>
   );
 };
 

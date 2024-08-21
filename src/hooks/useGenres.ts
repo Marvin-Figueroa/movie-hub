b6d617c44f096e3f78aec18136ba7a2004899108
Marvin Figueroa <marvinfigueroa2012@gmail.com>
2024-08-21 14:18:06 -0600

    Remove unused property from Genre interface

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -3,7 +3,6 @@ import genres from "../data/genres";
 export interface Genre {
   id: number;
   name: string;
-  imgSrc: string;
 }
 
 export const useGenres = () => ({ data: genres, loading: false, error: null });
